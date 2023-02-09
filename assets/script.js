816003f1649f63456052ef6e2a5a1ed72b5bce7d
SaintOfMirren <drew.miller2015@gmail.com>
2023-02-08 21:29:35 -0800

    added a timer, the rest of the questions and functions to move between the questions

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,97 @@
-const quizQuestions = document.getElementById("quiz-questions");
+// const quizQuestions = document.getElementById("quiz-questions");
+
+// var countdown = 30;
+// var expired = setInterval(function () {
+//     var timer = document.getElementById("timer").innerHTML = countdown;
+//     timer--;
+//     var showScore = document.getElementById("score");
+//     if (countdown == 0) {
+//         question1.style.display = "none";
+//         question2.style.display = "none";
+//         question3.style.display = "none";
+//         question4.style.display = "none";
+//         question5.style.display = "none";
+//         showScore.style.display = "block";
+//         timer.innerHTML = "expired!";
+//     } else {
+//         if (countdown < 10) {
+//             countdown = 0 + '' + countdown;
+//         }
+//         countdown -= 1;
+//     }
+
+// }, 1000);
 
 
 
 function startFunction() {
-    var questionOne = document.getElementById("question1");
-    questionOne.style.display = "block";
+    var question1 = document.getElementById("question1");
+    question1.style.display = "block";
+    var countdown = 30;
+    timer.style.display = "block";
+    var expired = setInterval(function () {
+        var timer = document.getElementById("timer").innerHTML = countdown;
+        timer--;
+        var showScore = document.getElementById("score");
+        if (countdown == 0) {
+            question1.style.display = "none";
+            question2.style.display = "none";
+            question3.style.display = "none";
+            question4.style.display = "none";
+            question5.style.display = "none";
+            showScore.style.display = "block";
+            prompt
+        } else {
+            if (countdown < 10) {
+                countdown = 0 + '' + countdown;
+            }
+            countdown -= 1;
+        }
+
+    }, 1000);
 }
 
-function questionTwo(event) {
+function questionTwo() {
     var question2 = document.getElementById("question2");
-    var questionOne = document.getElementById("question1");
+    var question1 = document.getElementById("question1");
     question2.style.display = "block";
-    questionOne.style.display = "none";
+    question1.style.display = "none";
 }
 
 function questionThree() {
     var question2 = document.getElementById("question2");
-    var questionOne = document.getElementById("question1");
+    var question1 = document.getElementById("question1");
     var question3 = document.getElementById("question3");
     question3.style.display = "block";
     question2.style.display = "none";
-    questionOne.style.display = "none";
+    question1.style.display = "none";
 }
 
-quizQuestions.addEventListener('click', function (e) {
-    questionTwo();
-    e.stopPropagation();
-});
+function questionFour() {
+    var question4 = document.getElementById("question4");
+    var question2 = document.getElementById("question2");
+    var question1 = document.getElementById("question1");
+    var question3 = document.getElementById("question3");
+    question4.style.display = "block";
+    question3.style.display = "none";
+    question2.style.display = "none";
+    question1.style.display = "none";
+}
+
+function questionFive() {
+    var question5 = document.getElementById("question5");
+    var question4 = document.getElementById("question4");
+    var question2 = document.getElementById("question2");
+    var question1 = document.getElementById("question1");
+    var question3 = document.getElementById("question3");
+    question5.style.display = "block"
+    question4.style.display = "none";
+    question3.style.display = "none";
+    question2.style.display = "none";
+    question1.style.display = "none";
+}
+
+// quizQuestions.addEventListener('click', function (e) {
+//     questionTwo();
+//     e.stopPropagation();
+// });
