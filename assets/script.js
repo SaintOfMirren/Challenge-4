b26aa3655bed6dfc21308198f77726570b932306
SaintOfMirren <drew.miller2015@gmail.com>
2023-02-15 21:34:06 -0800

    tried updating the JS to include a stop button, but now I broke the timer

--- assets/script.js
+++ assets/script.js
@@ -22,33 +22,51 @@
 
 // }, 1000);
 
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
+var question3 = document.getElementById("question3");
+var question4 = document.getElementById("question4");
+var question5 = document.getElementById("question5");
+var countdown;
+var startButton = document.getElementById("startButton");
+var stopButton = document.getElementById("")
+var timer = document.getElementById("timer");
+startButton.addEventListener("click", startCountdown);
+// stopButton.addEventListener("click", function() {
+//     clearInterval(countdown);
+// });
+
+function startCountdown() {
+    var secondsLeft = 30;
+    if (startFunction) {
+        var showScore = document.getElementById("score");
+        countdown = setInterval(function() {
+            if (secondsLeft == 0) {
+                clearInterval(countdown);
+                question1.style.display = "none";
+                question2.style.display = "none";
+                question3.style.display = "none";
+                question4.style.display = "none";
+                question5.style.display = "none";
+                showScore.style.display = "block";
+            } else {
+                if (secondsLeft < 10) {
+                    secondsLeft = 0 + '' + secondsLeft;
+                    timer.style.display = "block";
+                    timer.textContent = secondsLeft;
+                    secondsLeft--;
+                }
+                secondsLeft -= 1;
+            }
+            
+        }, 1000);
+    }
+}
 
 
 function startFunction() {
     var question1 = document.getElementById("question1");
     question1.style.display = "block";
-    var countdown = 30;
-    timer.style.display = "block";
-    var expired = setInterval(function () {
-        var timer = document.getElementById("timer").innerHTML = countdown;
-        timer--;
-        var showScore = document.getElementById("score");
-        if (countdown == 0) {
-            question1.style.display = "none";
-            question2.style.display = "none";
-            question3.style.display = "none";
-            question4.style.display = "none";
-            question5.style.display = "none";
-            showScore.style.display = "block";
-            prompt
-        } else {
-            if (countdown < 10) {
-                countdown = 0 + '' + countdown;
-            }
-            countdown -= 1;
-        }
-
-    }, 1000);
 }
 
 function questionTwo() {
@@ -91,7 +109,29 @@ function questionFive() {
     question1.style.display = "none";
 }
 
-// quizQuestions.addEventListener('click', function (e) {
-//     questionTwo();
-//     e.stopPropagation();
-// });
+function finalQuestion() {
+    var question5 = document.getElementById("question5");
+    var question4 = document.getElementById("question4");
+    var question2 = document.getElementById("question2");
+    var question1 = document.getElementById("question1");
+    var question3 = document.getElementById("question3");
+    var scoreResults = document.getElementById("score");
+    question5.style.display = "none"
+    question4.style.display = "none";
+    question3.style.display = "none";
+    question2.style.display = "none";
+    question1.style.display = "none";
+    timer.style.display = "none";
+    scoreResults.style.display = "block";
+    setTimeout(function() {
+
+    })
+
+};
+
+function saveText() {
+    var text = prompt("Please enter your initials");
+    if (text !== null && text !== "") {
+      localStorage.setItem("savedText", text);
+    }
+};
